fd2624908a734ad5e6fcbbee22e8ec72c2ab234e
Rick Moynihan <rick.m@swirrl.com>
2021-10-13 16:14:23 +0100

    Change specs

--- src/swirrl/dogstatsd/specs.clj
+++ src/swirrl/dogstatsd/specs.clj
@@ -3,34 +3,31 @@
             [swirrl.dogstatsd :as datadog])
   (:import [java.net DatagramSocket InetSocketAddress]))
 
-(create-ns 'swirrl.dogstatsd.config)
-(alias 'dogconfig 'swirrl.dogstatsd.config)
-
-(s/def ::dogconfig/endpoint string?)
+(s/def ::endpoint string?)
 
 (s/def ::socket #(instance? DatagramSocket %))
 (s/def ::addr #(instance? InetSocketAddress %))
 
 (s/def ::tag-pair (s/tuple keyword? any?))
 
-(s/def :swirrl.dogstatsd/tags
+(s/def ::tags
   (s/nilable (s/every-kv keyword? any?)))
 
-(s/def ::dogconfig/client
-  (s/keys :req-un [::config/endpoint] :opt-un [:swirrl.dogstatsd/tags]))
+(s/def ::client-config
+  (s/keys :req-un [::endpoint] :opt-un [::tags]))
 
-(s/def :swirrl.dogstatsd/client
-  (s/keys :req-un [::socket ::addr] :opt-un [:swirrl.dogstatsd/tags]))
+(s/def ::client
+  (s/keys :req-un [::socket ::addr] :opt-un [::tags]))
 
-(defn metric-name? [metric]
+(defn- metric-name? [metric]
   (re-matches #"[a-zA-Z][a-zA-Z0-9_.]*" (datadog/render-metric-key metric)))
 
-(defn metric-length? [metric]
+(defn- metric-length? [metric]
   (< (count (datadog/render-metric-key metric)) 200))
 
-(s/def :swirrl.dogstatsd/metric (s/and #(satisfies? datadog/RenderMetric %)
-                                       metric-name?
-                                       metric-length?))
+(s/def ::metric-id (s/and #(satisfies? datadog/RenderMetric %)
+                          metric-name?
+                          metric-length?))
 
 (def metric-types #{"g" "c" "h"})
 
@@ -38,14 +35,14 @@
                             (comp not ratio?)))
 
 (s/fdef datadog/configure
-  :args (s/cat :opts ::dogconfig/client)
-  :ret :swirrl.dogstatsd/client)
+  :args (s/cat :opts ::client-config)
+  :ret ::client)
 
 (s/fdef datadog/format-metric
-  :args (s/cat :tags :swirrl.dogstatsd/tags
-               :metric :swirrl.dogstatsd/metric
+  :args (s/cat :tags ::tags
+               :metric ::metric-id
                :type metric-types
                :value any?
-               :tags :swirrl.dogstatsd/tags
+               :tags ::tags
                :sample-rate ::sample-rate)
   )
