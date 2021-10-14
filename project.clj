4740cfa2942302934df06ce1f2843c4535216be0
Rick Moynihan <rick.m@swirrl.com>
2021-10-14 08:59:22 +0100

    Remove old lein project.clj

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject cognician/dogstatsd-clj "0.1.2"
-  :description "Clojure client for DogStatsD, Datadog’s StatsD agent"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :url "https://github.com/Cognician/dogstatsd-clj"
-  :dependencies [[org.clojure/clojure "1.7.0"]])
